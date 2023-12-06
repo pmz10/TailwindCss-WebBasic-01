2f35edf8a21ec7554b488dac0f7b3936164ce7d7
pmz10 <welcome_to_pmz@hotmail.com>
2023-12-06 14:15:33 -0600

    animate-bg-banner

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Banner = () => {
   return (
     <section className="container w-11/12 mx-auto">
-      <div id="banner" className="w-full h-28 border-8 border-white mx-auto overflow-hidden bg-black shadow m-5 bg-banner bg-banner-position">
+      <div id="banner" className="w-full h-28 border-8 border-white mx-auto overflow-hidden bg-black shadow m-5 bg-banner bg-banner-position animate-bg-banner">
         <h1 className="bebas text-white block text-4xl font-normal tracking-wider m-7 mx-auto text-center drop-shadow-md ">Cursos de Desarrollo Web</h1>
       </div>
     </section>
